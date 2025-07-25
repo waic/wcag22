0412351f1189c4c92e1c9c93f480deb051da7177
Kazuhiko Tsuchiya <kaz.tsuchiya@gmail.com>
2025-07-25 15:51:35 +0900

    WCAG 2.2 テクニック集: 未訳のテクニックに冒頭訳註を入れる

---
+++ techniques/untranslated-note.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const targetAnchor = document.querySelector('.untranslated-note a');
+    if (!targetAnchor) return;
+
+    const pathParts = window.location.pathname.split('/');
+    const partsLength = pathParts.length;
+
+    const parentDir = partsLength >= 3 ? pathParts[partsLength - 2] : '';
+    const filename = partsLength >= 2 ? pathParts[partsLength - 1] : '';
+
+    const combinedPath = parentDir && filename ? `${parentDir}/${filename}` : filename;
+
+    targetAnchor.href = targetAnchor.href + combinedPath;
+});
+
+/*
+この JavaScript ファイルは、WCAG 2.2 テクニック集が完全に日本語訳されるまでの、仮のスクリプトです。
+WCAG 2.2 テクニック集が完全に日本語訳されたら、この .js ファイルを削除し、
+また各ページの head 要素内にある「<script src="untranslated-note.js"></script>」という一行を削除してください。
+併せて、各ページの main 要素の直下にある「<div class="untranslated-note">」も不要になるので削除してください。
+*/
